f6d73435e2dde20bfd7bb011ff0922cfa0e48d1c
liliping.fe <liliping.fe@bytedance.com>
2019-11-05 20:46:33 +0800

    fix: file.js
    Change-Id: I1b6b4e60043539b1c53b035a2368890c6526214e

--- bin/file.js
+++ bin/file.js
@@ -1,7 +1,14 @@
+import { existsSync } from "fs";
+
 const path = 'path';
 
 console.log('GITHUB_WORKSPACE: ', process.env.GITHUB_WORKSPACE)
 
 const filename = '.github/ISSUE_TEMPLATE.md'
 
-console.log('filename join', join(process.env.GITHUB_WORKSPACE, filename))
+const pathJoin = path.join(process.env.GITHUB_WORKSPACE, filename)
+console.log('filename join', pathJoin)
+
+const isExist = existsSync(pathJoin)
+
+console.log('isEcist', isExist);
