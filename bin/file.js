e42dd047a6da45092982b0485d71b37ad0a142c5
liliping.fe <liliping.fe@bytedance.com>
2019-11-05 20:53:31 +0800

    pwd path
    Change-Id: I5d274c9faea9eac9b582539373aed3aa07001392

--- bin/file.js
+++ bin/file.js
@@ -1,6 +1,21 @@
 var { existsSync } =  require("fs");
 var path =  require("path");
 
+const spawn = require('child_process').spawn;
+const pwd = spawn('pwd');
+
+pwd.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+pwd.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+pwd.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
 console.log('GITHUB_WORKSPACE: ', process.env.GITHUB_WORKSPACE)
 
 const filename = '.github/ISSUE_TEMPLATE.md'
