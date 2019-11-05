a3c4466c4e19ea13ea60d10a411fbce33c4e822f
liliping.fe <liliping.fe@bytedance.com>
2019-11-05 19:59:38 +0800

    init
    Change-Id: If0e7f7d1fc2f2b72391d7cc4edb88543ae1d06ae

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,7 @@
+---
+title: Someone just pushed
+body: #title \n <div>div</div>
+labels: bug, enhancement
+---
+
+Someone just pushed, oh no! Here's who did it: {{ payload.sender.login }}
---
+++ index.ts
@@ -0,0 +1,61 @@
+import Koa, { Context } from 'koa'
+const app = new Koa()
+
+app.use( async ( ctx ) => {
+  if ( ctx.url === '/' && ctx.method === 'GET' ) {
+    // 当GET请求时候返回表单页面
+    let html = `
+      <h1>koa2 request post demo</h1>
+      <form method="POST" action="/post">
+        <p>userName</p>
+        <input name="userName" /><br/>
+        <p>nickName</p>
+        <input name="nickName" /><br/>
+        <p>email</p>
+        <input name="email" /><br/>
+        <button type="submit">submit</button>
+      </form>
+    `
+    ctx.body = html
+  } else if ( ctx.url === '/post' && ctx.method === 'POST' ) {
+    // 当POST请求的时候，解析POST表单里的数据，并显示出来
+    let postData = await parsePostData( ctx )
+    ctx.body = postData
+  } else {
+    // 其他请求显示404
+    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+  }
+})
+
+// 解析上下文里node原生请求的POST参数
+function parsePostData( ctx: Context ) {
+  return new Promise((resolve, reject) => {
+    try {
+      let postData = "";
+      ctx.req.addListener('data', (data) => {
+        postData += data
+      })
+      ctx.req.addListener("end",function(){
+        let parseData = parseQueryStr( postData )
+        resolve( parseData )
+      })
+    } catch ( err ) {
+      reject(err)
+    }
+  })
+}
+
+// 将POST请求参数字符串解析成JSON
+function parseQueryStr( queryStr: string ) {
+  let queryData = {}
+  let queryStrList = queryStr.split('&')
+  for (let queryStr of queryStrList) {
+    let itemList = queryStr.split('=')
+    queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
+  }
+  return queryData
+}
+
+app.listen(3000, () => {
+  console.log('[demo] request post is starting at port 3000')
+})
