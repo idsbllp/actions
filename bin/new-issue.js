2a483bd36cfe49f315bb17107c7a9e9ccf98ae76
liliping.fe <liliping.fe@bytedance.com>
2019-11-05 21:03:43 +0800

    issue js
    Change-Id: I706e8515b19a42d5264dd389889192133c90ceb9

---
+++ bin/new-issue.js
@@ -0,0 +1,62 @@
+const core = require('@actions/core')
+const { Toolkit } = require('actions-toolkit')
+const fm = require('front-matter')
+const nunjucks = require('nunjucks')
+const dateFilter = require('nunjucks-date-filter')
+
+function listToArray (list) {
+  if (!list) return []
+  return Array.isArray(list) ? list : list.split(', ')
+}
+
+Toolkit.run(async tools => {
+  const template = core.getInput('filename') || '.github/ISSUE_TEMPLATE.md'
+  const env = nunjucks.configure({ autoescape: false })
+  env.addFilter('date', dateFilter)
+
+  const templateVariables = {
+    ...tools.context,
+    date: Date.now()
+  }
+
+  // Get the file
+  tools.log.debug('Reading from file', template)
+  const file = tools.getFile(template)
+
+  // Grab the front matter as JSON
+  const { attributes, body } = fm(file)
+  tools.log(`Front matter for ${template} is`, attributes)
+
+  const templated = {
+    body: env.renderString(body, templateVariables),
+    title: env.renderString(attributes.title, templateVariables)
+  }
+
+  tools.log.debug('Templates compiled', templated)
+  tools.log.info(`Creating new issue ${templated.title}`)
+
+  // Create the new issue
+  try {
+    const issue = await tools.github.issues.create({
+      ...tools.context.repo,
+      ...templated,
+      assignees: listToArray(attributes.assignees),
+      labels: listToArray(attributes.labels),
+      milestone: attributes.milestone
+    })
+
+    tools.log.success(`Created issue ${issue.data.title}#${issue.data.number}: ${issue.data.html_url}`)
+  } catch (err) {
+    // Log the error message
+    tools.log.error(`An error occurred while creating the issue. This might be caused by a malformed issue title, or a typo in the labels or assignees. Check ${template}!`)
+    tools.log.error(err)
+
+    // The error might have more details
+    if (err.errors) tools.log.error(err.errors)
+
+    // Exit with a failing status
+    tools.exit.failure()
+  }
+}, {
+  secrets: ['GITHUB_TOKEN']
+})
